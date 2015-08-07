89d2208fbbe274e93734a40e4644e3eb87249867
Thembelani Momoti <thembelani@digital.pepkorit.com>
2015-08-07 14:59:23 +0200

    Not running. cannot find partials

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ var fs = require('fs');
 function on404Request(response){
 	response.writeHead(404, {'content-Type':'text/plain'});
 	response.write('Error 404: Pages cannot be found');
+	response.end();
 }
 
 function onRequest(request, response){
 	if(request.method =='GET'&& request.url =='/'){
 		response.writeHead(200, {'content-Type':'text/html'});
-		fs.createReadStream('./index.html').pipe(response);
+		//response.write('hello');
+		//response.end();
+		fs.createReadStream('index.html').pipe(response);
 	}else{
 		on404Request(response);
 	}
@@ -18,4 +21,4 @@ function onRequest(request, response){
 
 
 http.createServer(onRequest).listen(3000);
-console.log("Server is running");
+console.log("Server is running...");
